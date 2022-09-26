b1f9b5ffa4d0636fe3c4238afe36112702f7c1b4
Kristina094 <102580856+Kristina094@users.noreply.github.com>
2022-09-26 16:16:10 +0800

    aaa

--- social/src/App.js
+++ social/src/App.js
@@ -1,9 +1,7 @@
+import { Person } from "@mui/icons-material";
+import Home from "./pages/home/Home";
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <Person/>
 }
 
 export default App;
