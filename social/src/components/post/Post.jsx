84d5ec077501a9bcd81f077fbe178182cf8f3d62
Kristina094 <sw21d017@mandakh.edu.mn>
2022-10-05 16:43:19 +0800

    sdfghj

--- social/src/components/post/Post.jsx
+++ social/src/components/post/Post.jsx
@@ -1,6 +1,7 @@
 import "./post.css"
 import {MoreVert} from "@mui/icons-material"
-export default function Post() {
+export default function Post({post}) {
+  console.log(post)
   return (
     <div className="post">
         <div className="postWrapper">
@@ -8,21 +9,21 @@ export default function Post() {
                 <div className="postTopLeft">
                     <img className="postProfileImg" src="/assets/person/1.jpeg" alt="" />
                     <span className="postUsername">Safak Kocaoglu</span>
-                    <span className="postDate">5 mins ago</span>
+                    <span className="postDate">ggfgdgdgdg</span>
                 </div>
                 <div className="postTopRight">
                     <MoreVert/>
                 </div>
             </div>
             <div className="postCenter">
-                <span className="postText">Hey! its my first post:)</span>
+                <span className="postText"> gfgfgfgfg fgfg gf</span>
                 <img className="postImg" src="assets/post/1.jpeg" alt="" />
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
                     <img className="likeIcon" src="assets/like.png" alt="" />
                     <img className="likeIcon" src="assets/heart.png" alt="" />
-                    <span className="postLikeCounter">32 people like it</span>
+                    <span className="postLikeCounter">10 people like it</span>
                 </div>
                 <div className="postBottomRight">
                     <span className="postCommentText">9 comments</span>
