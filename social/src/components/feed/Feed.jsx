46b5cc409d3bb142153a21e49fc8e4c3559198b3
Kristina094 <sw21d017@mandakh.edu.mn>
2022-10-10 01:41:24 +0800

    dfsdfs

--- social/src/components/feed/Feed.jsx
+++ social/src/components/feed/Feed.jsx
@@ -8,10 +8,9 @@ export default function Feed() {
     <div className="feed">
         <div className="feedWrapper">
           <Share/>
-          {Posts.map((p)=>(
+          {Posts.map((p) => (
             <Post key={p.id} post={p}/>
           ))}
-          <Post/>
         </div>
         </div>
   )
--- social/src/components/rightbar/Rightbar.jsx
+++ social/src/components/rightbar/Rightbar.jsx
@@ -1,4 +1,7 @@
 import "./rightbar.css"
+import { Users } from "../../dummyData"
+
+
 
 export default function Rightbar() {
   return (
@@ -13,13 +16,8 @@ export default function Rightbar() {
         <img className="rightbarAd" src="assets/ad.png" alt="" />
         <h4 className="rightbarTitle">Online Friende</h4>
         <ul className="rightbarFriendList">
-          <li className="rightbarFriend">
-            <div className="rightbarProfileImgContainer">
-              <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt="" />
-              <span className="rightbarOnline"></span>
-            </div>
-            <span className="rightbarUsername">John Carter</span>
-          </li>
+          {Users.map(u=>(
+          <online key={u.id} user={u}/>))}
         </ul>
       </div>
     </div>
