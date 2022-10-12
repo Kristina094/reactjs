1d9696afbb96a01dfb525651f286c0ffb3a79ef8
Kristina094 <sw21d017@mandakh.edu.mn>
2022-10-13 02:39:56 +0800

    bichleg2 duusav

--- social/src/App.js
+++ social/src/App.js
@@ -1,8 +1,7 @@
 import  Home  from "./pages/home/Home";
-import Profile from "./pages/profile/Profile";
 // import { Person } from "@mui/icons-material";
 function App() {
-  return <Profile/>
+  return <Home/>
 }
 
 export default App;
