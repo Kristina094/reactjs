6fa0425952f7ab5c286fc7fa2bb3be33e52a428e
Kristina094 <102580856+Kristina094@users.noreply.github.com>
2022-10-18 14:37:21 +0800

    bichleg3fdsfsdfsfd

--- weather/src/components/search/search.js
+++ weather/src/components/search/search.js
@@ -1,20 +1,42 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geaApioptions } from "../../api";
 
-const Search =({onSearchChange}) =>{
+const Search = ({ onSearchChange }) => {
+    const [search, setSearch] = useState(null);
+
+    const loadOptions = (inputValue) => {
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix = ${inputValue}`,
+            geaApioptions
+        )
+            .then(response => response.json())
+            .then(response => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.logitude}`,
+                            label: `${city.name}, ${city.countryCode}`,
+                        }
+                    })
+                }
+            })
+            .catch(err => console.error(err));
+    };
 
 
-    const[search,setSearch]=useState(null);
 
-    const handleOnChange=(searchData)=>{
+    const handleOnChange = (searchData) => {
         setSearch(searchData);
+        onSearchChange(searchData);
     }
-    return(
+    return (
         <AsyncPaginate
-        placeholder="Search for city"
-        debounceTimeout={600}
-        value={search}
-        onChange={handleOnChange}
+            placeholder="Search for city"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
         />
     )
 }
