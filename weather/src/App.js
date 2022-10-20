9f870de9e5a223ca69be53ac827d4f55a6e59e18
Kristina094 <102580856+Kristina094@users.noreply.github.com>
2022-10-21 00:53:38 +0800

    dfghjkl

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import Search from './components/search/search';
+import CurrentWeather from './components/current-weather/current-weather';
+import { WEATHER_API_KEY, WEATHER_API_URL } from './api';
+
+
+
 function App() {
 
   const handleOnSearchChange=(searchData)=>{
-    console.log(searchData);
+    const[lat, lon]= searchData.value.split(" ");
+
+    const currentWeatherFeth = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+
+
+    api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
   }
   return (
   <div className='container'>
     <Search onSearchChange={handleOnSearchChange}/>
+    <CurrentWeather/>
   </div>
   );
 }
