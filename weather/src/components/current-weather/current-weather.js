6b0d07f2b1d855f3ce60595bffc7c39c5f96c264
Shagj Batjargal <>
2022-10-21 15:27:11 +0800

    hkljk

--- weather/src/components/current-weather/current-weather.js
+++ weather/src/components/current-weather/current-weather.js
@@ -1,14 +1,14 @@
 import "./current-weather.css"
 
-const CurrentWeather = () => {
+const CurrentWeather = ({data}) => {
     return (
         <div className="weather">
             <div className="top">
                 <div>
-                    <p className="city">Belguim</p>
-                    <p className="weather-deescription">Sunny</p>
+                    <p className="city">{data.city}</p>
+                    <p className="weather-deescription">{data.weather[0].description}</p>
                 </div>
-                <img alt="weather" className="weather-icon" src="icons/01d.png" />
+                <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}png`} />
             </div>
             <div className="bottom">
                 <p className="temperature">18°C</p>
