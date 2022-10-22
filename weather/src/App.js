eb4f186fa90a5423fa4b676c378d7a6dd62de073
Kristina094 <102580856+Kristina094@users.noreply.github.com>
2022-10-22 17:56:24 +0800

    dfsfsdg

--- weather/src/App.js
+++ weather/src/App.js
@@ -3,6 +3,7 @@ import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
 import { WEATHER_API_KEY, WEATHER_API_URL } from './api';
 import { useState } from 'react';
+import Forecast from './components/forecast/forecast';
 
 
 
@@ -13,18 +14,20 @@ function App() {
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
 
-    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
-    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
 
     Promise.all([currentWeatherFetch, forecastFetch])
-      .then(async(response)=>{
-        const weatherResponse= await response[0].json();
-        const forecastResponse= await response[1].json();
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
 
-        setCurrentWeather({city:searchData.label, ...weatherResponse});
-        setForecast({city:searchData.label, ...forecastResponse});
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
       })
-      .catch((err)=>console.log(err));
+      .catch((err) => console.log(err));
 
   }
 
@@ -34,7 +37,8 @@ function App() {
   return (
     <div className='container'>
       <Search onSearchChange={handleOnSearchChange} />
-      <CurrentWeather />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {Forecast && <Forecast data={forecast}/>}
     </div>
   );
 }
